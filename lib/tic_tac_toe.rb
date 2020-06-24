4b81076c9e826827acb693399901233c5c84f883
ndorazil <nathandorazil@live.com>
2020-06-23 22:02:01 -0600

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -8,7 +8,7 @@ class TicTacToe
         [0,3,6],
         [0,4,8],
         [2,5,8],
-        [6,4,2],
+        [2,4,6],
         [1,4,7]
     ]
 
@@ -22,6 +22,7 @@ class TicTacToe
             i += 1
         end
         @board = Array.new(9, " ")
+        # @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     end
 
     def display_board
@@ -32,16 +33,16 @@ class TicTacToe
         puts (" #{board[6]} | #{board[7]} | #{board[8]} ")
     end
 
-    def input_to_index(x)
-        @index = x.to_i - 1
+    def input_to_index(input)
+        @index = input.to_i - 1
     end
 
-    def move(index, token="X")
-        @board[index] = token
+    def move(index, token)
+        board[index] = token
     end
 
     def position_taken?(index)
-        @board[index] == "X" || @board[index] == "O"
+        board[index] == "X" || board[index] == "O"
     end
 
     def valid_move?(index)
@@ -49,38 +50,66 @@ class TicTacToe
     end
 
     def turn_count
-        @board.count { |space| space == "X" || space == "O" }
+        board.count { |space| space == "X" || space == "O"}
     end
 
     def current_player
-        
+        turn_count.even? ? "X" : "O"
     end
 
     def turn
+        puts "Please enter a number between 1 and 9:"
         input = gets.strip
         index = input_to_index(input)
-        if input == (1...9)
-            
+        if valid_move?(index)
+            move(index, current_player)
+            display_board
+        else
+            turn
         end
     end
 
     def won?
+        WIN_COMBINATIONS.find do |combo|
+            position_1_index = combo[0]
+            position_2_index = combo[1]
+            position_3_index = combo[2]
 
+            position_1_token = @board[position_1_index]
+            position_2_token = @board[position_2_index]
+            position_3_token = @board[position_3_index]
+
+            position_1_token == position_2_token &&
+            position_2_token == position_3_token &&
+            position_taken?(position_1_index)
+        end
     end
 
     def full?
-
+        @board.all?{|token| token == "X" || token == "O"}
     end
 
     def draw?
-
+        full? && !won?
     end
 
     def over?
-
+        !!(won? or draw?)
     end
 
     def winner
+        won? && board[won?[0]]
+    end
 
+    def play
+        until over?
+            turn
+        end
+
+        if won?
+            puts "Congratulations #{winner}!"
+        else
+            puts "Cat's Game!"
+        end
     end
 end
